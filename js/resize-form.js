8cb01d50717c5038d4e0ab72d2ab60698dac9ad1
Natalia Yorkina <yorkinan@gmail.com>
2015-09-27 00:52:55 +0300

    validation calc fix

--- js/resize-form.js
+++ js/resize-form.js
@@ -31,45 +31,47 @@
   var topSize = document.getElementById('resize-y');
   var previewImage = document.querySelector('.resize-image-preview');
 
-  var imgHeight = 0, imgWidth = 0;
+  var imgHeight = 0, 
+      imgWidth = 0;
 
   //считываем факт загрузки картинки
-  previewImage.addEventListener('load', function(evt) {
+  previewImage.addEventListener('load', function() {
 
   //получаем размеры каринки после загрузки
-    imgHeight = this.clientHeight;
     imgWidth = this.clientWidth;
+    console.info('imgWidth', imgWidth);
+    imgHeight = this.clientHeight;
+    console.info('imgHeight', imgHeight);
 
   //находим максимальное значение стороны квадрата
 
     squareSize.max = Math.min(imgWidth, imgHeight);
-    console.log(imgWidth + " и " + imgHeight + " макс: " + squareSize.max );
+    console.log(imgWidth + " и " + imgHeight + " макс: " + squareSize.max);
+    
+    leftSize.max = imgWidth - squareSize.max;
+    topSize.max = imgHeight - squareSize.max;
   });
 
   //находим максимальное значение отступа, если есть отступ слева
-  leftSize.addEventListener ('change', function(evt) {
-
-    if (leftSize >= squareSize.max ){
-      leftSize.max = (imgWidth - squareSize.max);
+  leftSize.addEventListener('change', function(){
+    if (!squareSize.value) {
+      this.max = squareSize.max - squareSize.min;
     } else {
-      leftSize.max = 0;
-    }
-    console.log (squareSize.max);
+      this.max = imgWidth - squareSize.value;
+    }  
   });
 
   //находим максимальное значение отступа, если есть отступ сверху
-  topSize.addEventListener ('change', function(evt) {
-
-    if (topSize >= squareSize.max ){
-      topSize.max = (imgHeight - squareSize.max);
+  topSize.addEventListener ('change', function(){
+    if (!squareSize.value) {
+      this.max = squareSize.max - squareSize.min;
     } else {
-      topSize.max = 0;
-    }
-    console.log (squareSize.max);
+      this.max = imgHeight - squareSize.value;
+    }  
   });
 
   //пересчитваем поля отступов, если ввели вручную размер и хотим с ним жить
-  squareSize.addEventListener ('change', function(evt) {
+  squareSize.addEventListener ('change', function() {
 
     if (squareSize.max > 1){
       topSize.max = (imgHeight - squareSize.value);
@@ -78,14 +80,3 @@
     console.log (squareSize.max);
   });
 })();
-
-
-
-
-
-
-
-
-
-
-
