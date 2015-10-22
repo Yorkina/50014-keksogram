5fd70d3ea284814d44604c4ebd93be2aa2ad70ad
Natalia Yorkina <yorkinan@gmail.com>
2015-10-23 00:05:35 +0300

    Travis bugs fix

--- js/resize-form.js
+++ js/resize-form.js
@@ -1,3 +1,5 @@
+'use strict';
+
 (function() {
   var uploadForm = document.forms['upload-select-image'];
   var resizeForm = document.forms['upload-resize'];
@@ -29,13 +31,12 @@
   var squareSize = document.getElementById('resize-size');
   var leftSize = document.getElementById('resize-x');
   var topSize = document.getElementById('resize-y');
-  var previewImage = document.querySelector('.resize-image-preview');
 
-  var imgHeight = 0, 
-      imgWidth = 0;
+  var imgHeight = 0;
+  var imgWidth = 0;
 
   //считываем факт загрузки картинки
-  previewImage.addEventListener('load', function() {
+  document.querySelector('.resize-image-preview').addEventListener('load', function() {
 
   //получаем размеры каринки после загрузки
     imgWidth = this.clientWidth;
@@ -49,38 +50,36 @@
   //находим максимальное значение стороны квадрата
 
     squareSize.max = Math.min(imgWidth, imgHeight);
-    console.log(imgWidth + " и " + imgHeight + " макс: " + squareSize.max);
-    
+
     leftSize.max = imgWidth - squareSize.max;
     topSize.max = imgHeight - squareSize.max;
   });
 
   //находим максимальное значение отступа, если есть отступ слева
-  leftSize.addEventListener('change', function(){
+  leftSize.addEventListener('change', function() {
     if (!squareSize.value) {
       this.max = squareSize.max - squareSize.min;
     } else {
       this.max = imgWidth - squareSize.value;
-    }  
+    }
   });
 
   //находим максимальное значение отступа, если есть отступ сверху
-  topSize.addEventListener ('change', function(){
+  topSize.addEventListener('change', function() {
     if (!squareSize.value) {
       this.max = squareSize.max - squareSize.min;
     } else {
       this.max = imgHeight - squareSize.value;
-    }  
+    }
   });
 
   //пересчитваем поля отступов, если ввели вручную размер и хотим с ним жить
-  squareSize.addEventListener ('change', function() {
+  squareSize.addEventListener('change', function() {
 
-    if (squareSize.max > 1){
+    if (squareSize.max > 1) {
       topSize.max = (imgHeight - squareSize.value);
       leftSize.max = (imgWidth - squareSize.value);
-    } 
-    console.log (squareSize.max);
+    }
   });
 
 })();
